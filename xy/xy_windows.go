6f1289b194f5bff4e08e5a82091d0095a8e877e4
algo <algo@163.com>
2024-10-11 08:56:21 +0800

    windows build

--- xy/xy_windows.go
+++ xy/xy_windows.go
@@ -6,10 +6,12 @@ import (
 	"l5proxy_cv/meta"
 )
 
-func (xy *XY) newTunMode(cfg *config.Config, handler meta.TunTransportHandler, bypass meta.Bypass) (meta.Local, error) {
+func (xy *XY) newTunMode(cfg *config.Config, handler meta.TunTransportHandler,
+	bypass meta.Bypass, protector func(fd uint64)) (meta.Local, error) {
 	_ = cfg
 	_ = handler
 	_ = bypass
+	_ = protector
 	return nil, fmt.Errorf("windows not support tun mode, only works at linux/unix")
 }
 
